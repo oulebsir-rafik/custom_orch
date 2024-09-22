32b31ef934951501cdb43ec208d046b1bbc28a24
rafik <oulebsir.rafik.enp@gmail.com>
2024-09-22 19:20:01 +0100

    feat: cleaned the task execution

--- src/dag.py
+++ src/dag.py
@@ -45,4 +45,24 @@ def organize_dag(dag_tasks: list[PyTask]):
 
     # reverse the list to get the correct order
     organized_execution.reverse()
-    return organized_execution
+
+    organize_dag = clean_dag(organized_execution)
+
+    return organize_dag
+
+
+def clean_dag(dag_tasks: list[list[PyTask]]):
+    """Remove duplicate tasks in the DAG."""
+    clean_dag = dag_tasks.copy()
+    # loop through the tasks in the DAG until the last one
+    for count_dag in range(len(dag_tasks) - 1):
+        actual_tasks = clean_dag[count_dag]
+        # loop through the next list of tasks
+        for count_next_dag in range(count_dag + 1, len(clean_dag)):
+            # NOTE: always copy an iterable when its length is going to change
+            for task in clean_dag[count_next_dag].copy():
+                # check if the task is in the actual list of tasks
+                if task in actual_tasks:
+                    clean_dag[count_next_dag].remove(task)
+
+    return clean_dag
